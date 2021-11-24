1965387da445f6636e193de0ffb9f61f5b4c9f4d
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2021-11-24 14:28:15 +0100

    initial commit

---
+++ template.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require (lib "racket/port"))
+
+(define day "00")
+
+(define file-contents
+  (string-split
+   (port->string (open-input-file (format "day-~a.in" day)) #:close? #t)
+   "\n"))
+
+(define reverse
+  (lambda (list) (foldl cons null list)))
+
+(define nal1 file-contents)
+
+(define nal2
+  (reverse
+   (for/list ([l file-contents])
+     (string-upcase l))))
+
+(call-with-output-file (format "day-~a-1.out" day) #:exists 'truncate
+  (lambda (out) (display (string-join nal1 "\n") out)))
+(call-with-output-file (format "day-~a-2.out" day) #:exists 'truncate
+  (lambda (out) (display (string-join nal2 "\n") out)))
