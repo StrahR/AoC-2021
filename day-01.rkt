e001511c799a1b2c3d114c109934a220226cee87
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2021-12-01 22:35:05 +0100

    renamed parameters

--- day-01.rkt
+++ day-01.rkt
@@ -7,11 +7,11 @@
 (define input
   (map string->number (aoc-read day)))
 
-(define (nal1 ll)
-  (count identity (mapb < ll (cdr ll))))
+(define (nal1 input)
+  (count identity (mapb < input (cdr input))))
 
-(define (nal2 ll)
-  (nal1 (mapb + ll (cdr ll) (cddr ll))))
+(define (nal2 input)
+  (nal1 (mapb + input (cdr input) (cddr input))))
 
 (aoc-write day 1 (nal1 input))
 (aoc-write day 2 (nal2 input))
