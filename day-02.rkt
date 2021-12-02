19fa34c008532fe940064a441872d899ad1023d8
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2021-12-02 08:58:39 +0100

    solved day 2 part 2

--- day-02.rkt
+++ day-02.rkt
@@ -7,7 +7,7 @@
 (define input
   (map (λ (s) (cons (car s) (string->number (cadr s))))
        (map (λ (s) (string-split s " "))
-            (aoc-read day #f))))
+            (aoc-read day))))
 
 (define (nal1 input)
   (for/fold ([pos 0] [depth 0] #:result (* pos depth))
@@ -17,7 +17,13 @@
       [(cons "down" n) (values pos (+ depth n))]
       [(cons "up" n) (values pos (- depth n))])))
 
-(define nal2 "")
+(define (nal2 input)
+  (for/fold ([aim 0] [pos 0] [depth 0] #:result (* pos depth))
+            ([command (in-list input)])
+    (match command
+      [(cons "forward" n) (values aim (+ pos n) (+ (* aim n) depth))]
+      [(cons "down" n) (values (+ aim n) pos depth)]
+      [(cons "up" n) (values (- aim n) pos depth)])))
 
 (aoc-write day 1 (nal1 input))
-; (aoc-write day 2 (nal2 input))
+(aoc-write day 2 (nal2 input))
