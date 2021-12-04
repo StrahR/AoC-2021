ff883db18f5905df90719ceec093890f76deb9ea
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2021-12-04 23:24:57 +0100

    fixed template

--- day-00.rkt
+++ day-00.rkt
@@ -6,10 +6,9 @@
 
 (define input (aoc-read day))
 
-(define nal1 input)
+(define (nal1 input) input)
 
-(define nal2
-  (reverse (map string-upcase input)))
+(define (nal2 input) input))
 
 (aoc-write day 1 (nal1 input))
 (aoc-write day 2 (nal2 input))
