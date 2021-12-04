bf0207c1bc4ecee002eec081c0d317428fdb4630
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2021-12-04 15:01:18 +0100

    solved day 4 part 2

--- day-04.rkt
+++ day-04.rkt
@@ -15,21 +15,11 @@
                    (list->array #(5 5) (map string->number (string-split bingo #:repeat? #t))))
                  (cdr in)))))
 
-; (display (~v input))
-; (display (~v bingos))
 
 (define (has-winning-row? bingo)
   (array-ormap (λ (s) (< s 0)) (array-axis-max bingo 1)))
 (define (has-winning-col? bingo)
   (array-ormap (λ (s) (< s 0)) (array-axis-max bingo 0)))
-; (< (array-axis-max bingo 1) 0))
-
-; (let ([arr (list->array #(3 2) '(-1 -2 1 -2 3 -4))])
-;   (display arr)
-;   (display (array-axis-max arr 1))
-;   (format "row: ~a; col: ~a"
-;           (has-winning-row? arr)
-;           (has-winning-col? arr)))
 
 (define (is-winning? bingo) (or (has-winning-row? bingo) (has-winning-col? bingo)))
 
@@ -52,7 +42,14 @@
     #:break (ormap is-winning? bingos)
     (values (map (mark k) bingos) k)))
 
-(define (nal2 input bingos) "")
+(define (nal2 input bingos)
+  (for/fold ([bingos bingos]
+             [bingus bingos]
+             [n 0]
+             #:result (* n (- (score (car bingus)) n)))
+            ([k (in-list input)])
+    #:break (andmap is-winning? bingos)
+    (values (map (mark k) bingos) (filter-not is-winning? bingos) k)))
 
-(aoc-write day 1 (nal1 input bingos) #t)
+(aoc-write day 1 (nal1 input bingos))
 (aoc-write day 2 (nal2 input bingos))
