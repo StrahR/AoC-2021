08f813c0de361dacfbe2338cac84acc7d4f62e2c
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2021-12-05 17:33:34 +0100

    refactored day 5

--- day-05.rkt
+++ day-05.rkt
@@ -16,35 +16,29 @@
     (pure (list x1 y1 x2 y2))))
 
 (define input (map (λ (s) (parse-result! (parse-string day05/p s))) (aoc-read day)))
-; (display input)
 
 (define (add-line vents a start end dir)
   (if (> start end) vents
       (match dir
-        ['→ (add-line (hash-update vents (cons a start) add1 0) a (add1 start) end dir)]
-        ['↓ (add-line (hash-update vents (cons start a) add1 0) a (add1 start) end dir)]
+        ['→ (add-line (hash-update vents (cons a start) add1 0)       a  (add1 start) end dir)]
+        ['↓ (add-line (hash-update vents (cons start a) add1 0)       a  (add1 start) end dir)]
         ['↘ (add-line (hash-update vents (cons a start) add1 0) (add1 a) (add1 start) end dir)]
         ['↗ (add-line (hash-update vents (cons start a) add1 0) (sub1 a) (add1 start) end dir)])))
 
-(define (nal1 input)
-  (count (cut > <> 1)
-         (hash-values
-          (for/fold/match (in-list input)
-            ([vents (hash)])
-            [(list x1 y1 x2 y2) #:when (= x1 x2) (add-line vents x1 (min y1 y2) (max y1 y2) '→)]
-            [(list x1 y1 x2 y2) #:when (= y1 y2) (add-line vents y1 (min x1 x2) (max x1 x2) '↓)]
-            [_ vents]))))
+(define (count-dangers input)
+  (count
+   (cut > <> 1)
+   (for/fold/match (in-list input)
+     ([vents (hash)] #:result (hash-values vents))
+     [(list x1 y1 x2 y2) #:when (= x1 x2) (add-line vents x1 (min y1 y2) (max y1 y2) '→)]
+     [(list x1 y1 x2 y2) #:when (= y1 y2) (add-line vents y1 (min x1 x2) (max x1 x2) '↓)]
+     [(list x1 y1 x2 y2) #:when (eq? (< x1 x2) (< y1 y2))
+                         (add-line vents (min x1 x2) (min y1 y2) (max y1 y2) '↘)]
+     [(list x1 y1 x2 y2) (add-line vents (max y1 y2) (min x1 x2) (max x1 x2) '↗)])))
 
-(define (nal2 input)
-  (count (cut > <> 1)
-         (hash-values
-          (for/fold/match (in-list input)
-            ([vents (hash)])
-            [(list x1 y1 x2 y2) #:when (= x1 x2) (add-line vents x1 (min y1 y2) (max y1 y2) '→)]
-            [(list x1 y1 x2 y2) #:when (= y1 y2) (add-line vents y1 (min x1 x2) (max x1 x2) '↓)]
-            [(list x1 y1 x2 y2) #:when (eq? (< x1 x2) (< y1 y2))
-                                (add-line vents (min x1 x2) (min y1 y2) (max y1 y2) '↘)]
-            [(list x1 y1 x2 y2) (add-line vents (max y1 y2) (min x1 x2) (max x1 x2) '↗)]))))
+(define (nal1 input)
+  (count-dangers (filter (match-lambda [(list x1 y1 x2 y2) (or (= x1 x2) (= y1 y2))]) input)))
+(define (nal2 input) (count-dangers input))
 
 (aoc-write day 1 (nal1 input))
 (aoc-write day 2 (nal2 input))
