79ab436af51b399954b87f9b54f1da1cf89f3fea
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2021-12-06 11:55:30 +0100

    refactored day 6

--- day-06.rkt
+++ day-06.rkt
@@ -6,30 +6,18 @@
 
 (define input (map string->number (string-split (car (aoc-read day)) ",")))
 
-(define (lanternfish-day input)
-  (let ([n (count (cut = 0 <>) input)])
-    (append (make-list n 8) (map (λ (n) (if (< n 0) 6 n)) (map sub1 input)))))
+(define (get-lanternfish-state input)
+  (for/fold ([acc (hash)]) ([f (in-list input)])
+    (hash-update acc f add1 0)))
 
-(define (nal1 input)
-  (for/fold ([lanternfish input] #:result (length lanternfish))
-            ([i (in-range 80)])
-    (lanternfish-day lanternfish)))
+(define (lanternfish-evolution input days)
+  (for/fold ([lanternfish (get-lanternfish-state input)]
+             #:result (apply + (hash-values lanternfish)))
+            ([i (in-range days)])
+    (let* ([n (hash-ref lanternfish i 0)]
+           [lanternfish (hash-update lanternfish (+ i 7) (cut + <> n) 0)]
+           [lanternfish (hash-update lanternfish (+ i 9) (cut + <> n) 0)])
+      (hash-remove lanternfish i))))
 
-(define (get-lanternfish h d)
-  (flatten (for/list ([f (in-hash-pairs h)])
-             (match f [(cons key value) (make-list value (- key d -1))]))))
-
-(define (nal2 input)
-  (let ([input (for/fold ([acc (hash)])
-                         ([f (in-list input)])
-                 (hash-update acc f add1 0))])
-    (for/fold ([lanternfish input] #:result (apply + (hash-values lanternfish)))
-              ([i (in-range 256)])
-      (let* ([n (hash-ref lanternfish i 0)]
-             [lanternfish (hash-set lanternfish i 0)]
-             [lanternfish (hash-update lanternfish (+ i 7) (cut + <> n) 0)]
-             [lanternfish (hash-update lanternfish (+ i 9) (cut + <> n) 0)])
-        lanternfish))))
-
-(aoc-write day 1 (nal1 input))
-(aoc-write day 2 (nal2 input))
+(aoc-write day 1 (lanternfish-evolution input  80))
+(aoc-write day 2 (lanternfish-evolution input 256))
